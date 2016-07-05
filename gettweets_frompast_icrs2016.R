c23156eeebb316055b0efa6dc08e289e97caca9d
thomas-keller <thomas.e.keller@gmail.com>
2016-07-05 15:32:37 -0400

    updated R script

--- gettweets_frompast_icrs2016.R
+++ gettweets_frompast_icrs2016.R
@@ -7,4 +7,8 @@ setup_twitter_oauth(cons_key,cons_sec, acc_tok, acc_sec)
 tw <- searchTwitter('#isemph', n = 1e4, since = '2016-06-22')
 tw3 <- searchTwitter('#icrs2016', n = 2e4, since = '2016-06-18') #11200
 
+t4 <- searchTwitter('#isemph', n = 1e4, since = '2016-06-22')
+t5 <- searchTwitter('#icrs2016', n = 2e4, since = '2016-06-18') #11200
+
 coral_tw<-twListToDF(tw3)
+write.write(coral_tw,file='isemph_tweets.csv',row.names=False)
